ba088e408b6ed58ae971d65ec72388e9f294825d
Jem1D <jackdharia@gmail.com>
2024-05-03 19:22:56 +0530

    new 10

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
-const express = require('express');
-const cors = require('cors'); // Import the cors package
-
+const express = require("express");
+const dotenv = require("dotenv");
+const emailRoutes = require("./routes/emailRoutes");
 const app = express();
 
-// Use the cors middleware
-app.use(cors());
+dotenv.config();
 
-// Define a route handler for the root URL
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+
+app.use((req, res, next) => {
+  res.header(
+  'Access-Control-Allow-Origin',
+  '*'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  
+  console.log('Request received:', req.method, req.url);
+  
+  next();
+  });
+
+
+
+app.use(express.json()); // Tell the server to accept JSON data from the frontend
+
+// Signup and login routes
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-// Start the Express server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.use("/email", emailRoutes);
+
+const PORT = process.env.PORT; 
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
