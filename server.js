803ec1bd7290ec1717e24d7028976296fc2a75de
Sahaj Bhadja <sahajbhadja10@gmail.com>
2024-05-03 14:57:29 +0530

    debugging the server issue

--- server.js
+++ server.js
@@ -2,26 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 const emailRoutes = require("./routes/emailRoutes");
 const app = express();
+const cors = require('cors');
+
 dotenv.config();
 
-// const cors = require("cors");
-// const corsOptions = {
-//   origin: "*", // Allow requests from any origin
-//   credentials: true, // Access-Control-Allow-Credentials header
-//   optionSuccessStatus: 200, // Set the success status code to 200
-// };
+app.use(cors({
+  origin: '*', // Allow requests from this origin
+  methods: ['GET', 'POST'], // Allow these HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
+}));
 
-// app.use(cors(corsOptions)); // Use the configured CORS options
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
 
 app.use(express.json()); // Tell the server to accept JSON data from the frontend
 
@@ -32,7 +22,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const PORT = process.env.PORT || 3000; // Set a default port if PORT environment variable is not provided
+const PORT = process.env.PORT; 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
